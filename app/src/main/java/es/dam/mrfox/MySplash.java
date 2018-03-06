311cddeab21d91aa59dfa409c16bdd566307b69c
borja <borja@borja.com>
2018-03-06 16:07:47 +0100

    UP 1

---
+++ app/src/main/java/es/dam/mrfox/MySplash.java
@@ -0,0 +1,14 @@
+package es.dam.mrfox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_main);
+
+    }
+}
