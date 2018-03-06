362268bb0bb8018505ddf6acf0279a594282d928
borja <borja@borja.com>
2018-03-06 20:30:20 +0100

    UP 5 Final

--- app/src/main/java/es/dam/mrfox/MyLogin.java
+++ app/src/main/java/es/dam/mrfox/MyLogin.java
@@ -30,4 +30,5 @@ public class MyLogin extends AppCompatActivity {
 
 
     }
+
 }
--- app/src/main/java/es/dam/mrfox/MyMain.java
+++ app/src/main/java/es/dam/mrfox/MyMain.java
@@ -1,13 +1,17 @@
 package es.dam.mrfox;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class MyMain extends AppCompatActivity {
+
+    SwipeRefreshLayout mSwRellay;
+    private View RltContenedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +19,35 @@ public class MyMain extends AppCompatActivity {
         setContentView(R.layout.activity_my_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+
+        mSwRellay = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        mSwRellay.setOnRefreshListener(refrescarVista);
+        RltContenedor = findViewById(R.id.contenedor);
+
+    }
+    
+    protected SwipeRefreshLayout.OnRefreshListener refrescarVista = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Snackbar mySnackbar = Snackbar.make(RltContenedor,"Lost your FOX?",Snackbar.LENGTH_LONG);
+            mySnackbar.setAction("UNDO", new MyUndoListener());
+            mySnackbar.show();
+            mSwRellay.setRefreshing(false);
+        }
+    };
+
+    public class MyUndoListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            // Code to undo the user's last action
+        }
+    }
+    
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_my_main,menu);
+        return true;
     }
 }
